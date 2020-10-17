372e8253cf6bf6a36c6418b68877c9183f197109
Kiki <pompom1396@gmail.com>
2020-10-17 16:15:11 +0200

    Ukoly 19. 10.

---
+++ praktikum/ukoly2/zrnka_ryze.py
@@ -0,0 +1,22 @@
+count = 1
+
+for i in range(63):
+    count = count * 2
+print(count)
+
+
+# 9223372036854775808 zrnek
+
+# Prumerne 50000 zrnek v 1 kg ryze
+# 1 kg ryze -> 1.24 L
+# Mame 228739626513.998 m³ ryze
+# Objem nakladniho vagonu 82,5 m³ (CD Cargo Eanos II.)
+# Potrebujeme 2772601534 vagonu
+# Delka vagonu 15740 mm -> 15.740 m
+# Celkem delka 43640748.137 km
+# Obvod zemekoule 40075 km
+# Nakladni vlak by 1088.977 krat obtocil zemekouli
+
+# Vzdalenost k Mesici je 384400 km
+# Vlak by dosahl na Mesic 133.5 krat
+# Minimalni zdalenost k Marsu je 54.6 milionu km -> vlak by skoro dosahl na Mars (obcas)
